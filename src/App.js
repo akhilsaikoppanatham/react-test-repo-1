25ab27a06376799bb6e8742f0142b0ba6d96a172
Akhil Sai Koppanatham <koppanatham.a@northeastern.edu>
2022-01-24 16:12:04 -0500

    Made changes to the text in the react landing page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         This is Akhil First react project on the new m1 macbook Air
         </p>
       
         <a
@@ -16,8 +16,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React bh
+          Welcome to Boston
         </a>
+        
+        
       </header>
     </div>
   );
